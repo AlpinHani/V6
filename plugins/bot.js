256d4addab5f9e21e1377dde1e7d4d78329c182c
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-03-10 09:03:17 +0700

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, participants }) => {
 conn.reply(m.chat, `Iya Kak? ${conn.getName(conn.user.jid)} Online Ketik Aja *#menu* Untuk Melihat List Menu *${conn.getName(conn.user.jid)}* :)\n\nSilahkan Ketik #menu`, 'conversation', { quoted: m, contextInfo: { externalAdReply :{
-mediaUrl: 'https://wa.me/62895603352610?text=Hai.com',
+mediaUrl: 'https://wa.me/62896255561617?text=Hai.com',
 mediaType: 2,
 title: `Simple Bot Whatsapp`,
-body: '© YUKIBOT',
+body: '© ZEUSBOTZ',
 thumbnailUrl: 'https://i.ibb.co/TYBsTsV/1890eb474e10.jpg',
 }}}) 
 }
